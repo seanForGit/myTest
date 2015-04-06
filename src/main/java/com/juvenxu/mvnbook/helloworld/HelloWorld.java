5735fd70fc681310bd63aa7548d8240e513b12ff
seanForGit <wuxiaomin0313@163.com>
2015-04-06 10:56:50 +0800

    提交测试

--- src/main/java/com/juvenxu/mvnbook/helloworld/HelloWorld.java
+++ src/main/java/com/juvenxu/mvnbook/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld
 {  
    public String sayHello()  
    {  
-     return "Hello Maven";  
+     return "Hello Maven3333";  
    }  
   
   public static void main(String[] args)  
